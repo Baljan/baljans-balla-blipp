af72fd1a6ce3afb20b9eab475938acae3e8b24db
Olle Vidner <olle@vidner.se>
2016-08-16 08:09:43 +0200

    Modified rfid_converter to talk to the new Kobra API

--- rfid_converter.php
+++ rfid_converter.php
@@ -1,16 +1,13 @@
 <?php
   require_once("settings.php");
   function get_liuid($rfid){
-    $ch = curl_init(API_URL);
-    curl_setopt($ch,CURLOPT_USERPWD, API_USERPWD);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "rfid_number=".$rfid);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER ,1);
-    curl_setopt($ch, CURLOPT_SSL_CIPHER_LIST, 'TLSv1');
-    $user = json_decode(curl_exec($ch),true);
+    $ch = curl_init(API_URL.$rfid."/");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Token ".API_TOKEN));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $user = json_decode(curl_exec($ch), true);
 
     if (empty($user)) {
-       throw new Exception("Can't find a user with rfid number: ".$rfid);
+      throw new Exception("Can't find a user with rfid number: ".$rfid);
     }
 
     return $user['liu_id'];
--- settings.default.php
+++ settings.default.php
@@ -1,12 +1,11 @@
 <?php
 // API Settings
 define("API_URL", "");
-define("API_USERPWD", "");
+define("API_TOKEN", "");
 
 // Db Settings
 define("DB_HOST", "");
 define("DB_NAME", "");
 define("DB_USER", "");
 define("DB_PWD", "");
-
 ?>
